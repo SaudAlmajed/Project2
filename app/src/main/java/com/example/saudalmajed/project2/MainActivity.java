f34216b442b2cc51a59389e2c3ccc0d28212fa11
SaudAlmajed <s.almajed96@gmail.com>
2017-07-22 17:45:25 +0300

    changed

--- app/src/main/java/com/example/saudalmajed/project2/MainActivity.java
+++ app/src/main/java/com/example/saudalmajed/project2/MainActivity.java
@@ -1,75 +1,48 @@
 package com.example.saudalmajed.project2;
-
-
         import android.os.Bundle;
         import android.support.v7.app.AppCompatActivity;
         import android.view.View;
-
         import android.widget.TextView;
-
 
 public class MainActivity extends AppCompatActivity {
     private int FirstTeam =0;
     private int secondTeam =0;
 
-    private TextView textView1,textView2;
-
+    private TextView scoreFirstTeam,scoreSecondTeam;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
-        textView1=((TextView)findViewById(R.id.textViewF));
-        textView2=((TextView)findViewById(R.id.textViewS));
-
-
-
-    }public void plusf1(View view){
-        FirstTeam=FirstTeam+1;
-        textView1.setText(""+FirstTeam);
+        scoreFirstTeam=((TextView)findViewById(R.id.textViewF));
+        scoreSecondTeam=((TextView)findViewById(R.id.textViewS));
 
     }
-
+    public void plusf1(View view){
+        FirstTeam=FirstTeam+1;
+        scoreFirstTeam.setText(""+FirstTeam);
+    }
     public void plusf2(View view){
         FirstTeam=FirstTeam+2;
-        textView1.setText(""+FirstTeam);}
+        scoreFirstTeam.setText(""+FirstTeam);}
 
     public void plusf3(View view){ FirstTeam=FirstTeam+3;
-        textView1.setText(""+FirstTeam);}
-
-
-
-
+        scoreFirstTeam.setText(""+FirstTeam);}
     public void pluss1(View view){
 
         secondTeam=secondTeam+1;
-        textView2.setText(""+secondTeam);
+        scoreSecondTeam.setText(""+secondTeam);
 }
     public void pluss2(View view){secondTeam=secondTeam+2;
-        textView2.setText(""+secondTeam);
+        scoreSecondTeam.setText(""+secondTeam);
     }
     public void pluss3(View view){ secondTeam=secondTeam+3;
-        textView2.setText(""+secondTeam);}
-
-
-
+        scoreSecondTeam.setText(""+secondTeam);
+    }
     public void reset(View view){
         FirstTeam=0;
         secondTeam=0;
-        textView2.setText(""+secondTeam);
-        textView1.setText(""+FirstTeam);
-
+        scoreSecondTeam.setText(""+secondTeam);
+        scoreFirstTeam.setText(""+FirstTeam);
     }
-
-
-
-
-
-
 }
-
-
-
