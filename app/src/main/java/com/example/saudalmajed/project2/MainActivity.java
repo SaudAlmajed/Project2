471f675758a72740e7746ba776ed4b9271296fa6
SaudAlmajed <s.almajed96@gmail.com>
2017-07-22 21:04:33 +0300

    changed

--- app/src/main/java/com/example/saudalmajed/project2/MainActivity.java
+++ app/src/main/java/com/example/saudalmajed/project2/MainActivity.java
@@ -7,42 +7,30 @@ package com.example.saudalmajed.project2;
 public class MainActivity extends AppCompatActivity {
     private int FirstTeam =0;
     private int secondTeam =0;
-
     private TextView scoreFirstTeam,scoreSecondTeam;
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         scoreFirstTeam=((TextView)findViewById(R.id.textViewF));
-        scoreSecondTeam=((TextView)findViewById(R.id.textViewS));
-
-    }
+        scoreSecondTeam=((TextView)findViewById(R.id.textViewS));}
     public void plusf1(View view){
         FirstTeam=FirstTeam+1;
-        scoreFirstTeam.setText(""+FirstTeam);
-    }
+        scoreFirstTeam.setText(""+FirstTeam);}
     public void plusf2(View view){
         FirstTeam=FirstTeam+2;
         scoreFirstTeam.setText(""+FirstTeam);}
-
     public void plusf3(View view){ FirstTeam=FirstTeam+3;
         scoreFirstTeam.setText(""+FirstTeam);}
     public void pluss1(View view){
-
         secondTeam=secondTeam+1;
-        scoreSecondTeam.setText(""+secondTeam);
-}
+        scoreSecondTeam.setText(""+secondTeam);}
     public void pluss2(View view){secondTeam=secondTeam+2;
-        scoreSecondTeam.setText(""+secondTeam);
-    }
+        scoreSecondTeam.setText(""+secondTeam);}
     public void pluss3(View view){ secondTeam=secondTeam+3;
-        scoreSecondTeam.setText(""+secondTeam);
-    }
+        scoreSecondTeam.setText(""+secondTeam);}
     public void reset(View view){
         FirstTeam=0;
         secondTeam=0;
         scoreSecondTeam.setText(""+secondTeam);
-        scoreFirstTeam.setText(""+FirstTeam);
-    }
+        scoreFirstTeam.setText(""+FirstTeam);}
 }
